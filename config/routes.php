6aa0553d5d92ba11a4a7d5e575b08b0dd85a6a4a
AdekWHATT <vyacheslaw.kilin@mail.ru>
2023-09-18 16:19:18 +0300

    controllers create

--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,9 @@
 <?php
 use App\Router\Route;
+use App\Controllers\MovieController;
+use App\Controllers\HomeController;
 
 return [
-    Route::get('/home', function () {
-        include_once APP_PATH . '/views/pages/home.php';
-    }),
-    Route::get('/movies', function () {
-        include_once APP_PATH . '/views/pages/movies.php';
-    })
+    Route::get('/home', [HomeController::class, 'index']),
+    Route::get('/movies',[MovieController::class, 'index'])
 ];
