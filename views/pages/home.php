849f92b200e0c0754f4d047279eb200bb1232656
AdekWHATT <vyacheslaw.kilin@mail.ru>
2023-09-19 16:41:21 +0300

    add templates html 1h20m

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,1 +1,11 @@
+<?php
+
+/**
+ * @var \App\Kernel\View\View $view
+ */
+
+$view->component('start');?>
+
 <h1>Home</h1>
+
+<?php $view->component('end');?>
--- views/pages/movies.php
+++ views/pages/movies.php
@@ -1,1 +1,10 @@
-<h1>Movies</h1>
+<?php
+/**
+ * @var \App\Kernel\View\View $view
+ */
+
+$view->component('start');?>
+
+    <h1>Movies</h1>
+
+<?php $view->component('end');?>
